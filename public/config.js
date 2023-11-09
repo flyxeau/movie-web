86070d69e4b0a3fca2051550b2a462a5d49ab0c4
Flyxo <77932389+flyxeau@users.noreply.github.com>
2023-11-09 21:08:40 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://simple-proxy.axeldrelon.workers.dev/",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.axeldrelon.workers.dev",
   VITE_TMDB_READ_API_KEY: "821308e84895c264c65af9c94c17967f"
 };
 
