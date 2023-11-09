0defd10cad2cc118c7187c2de8d826823a9ee6c8
Flyxo <77932389+flyxeau@users.noreply.github.com>
2023-11-09 17:13:31 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://worker-flyxo-movie.axeldrelon.workers.dev",
+  VITE_TMDB_READ_API_KEY: "821308e84895c264c65af9c94c17967f"
 };
+
